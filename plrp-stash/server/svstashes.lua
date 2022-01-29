cd65ea5674cc594610b77c7c1039480379d3d4f9
Dusty1818 <doehldrich@gmail.com>
2022-01-28 22:28:12 -0600

    Add files via upload

---
+++ plrp-stash/server/svstashes.lua
@@ -0,0 +1,248 @@
+Config = {}
+Config.Stash = {}
+
+
+    Config.Stash[#Config.Stash + 1] = {
+        StashEntry = vector3(803.7325, -103.967, 80.61356),
+        RequiredPin = 1818,
+        ID = 1,
+        distance = 2,
+}
+
+    Config.Stash[#Config.Stash + 1] = {
+        StashEntry = vector3(150.61289978027, 322.69192504883, 112.33365631104),
+        RequiredPin = 1998,
+        ID = 2,
+        distance = 2,
+}
+
+    Config.Stash[#Config.Stash + 1] = {
+        StashEntry = vector3(-70.384872436523, 359.50149536133, 112.54649353027),
+        RequiredPin = 1028,
+        ID = 3,
+        distance = 2,
+}
+
+    Config.Stash[#Config.Stash + 1] = {
+        StashEntry = vector3(780.86968994141, 1296.5561523438, 361.36184692383),
+        RequiredPin = 0413,
+        ID = 4,
+        distance = 2,
+}
+
+    Config.Stash[#Config.Stash + 1] = {
+        StashEntry = vector3(967.17565917969, -1858.4056396484, 31.196933746338),
+        RequiredPin = 0732,
+        ID = 5,
+        distance = 2,
+}
+
+    Config.Stash[#Config.Stash + 1] = {
+        StashEntry = vector3(-520.64532470703, -615.11834716797, 26.529928207397),
+        RequiredPin = 2565,
+        ID = 6,
+        distance = 2,
+}
+
+    Config.Stash[#Config.Stash + 1] = {
+        StashEntry = vector3(-2307.0485839844, 269.08059692383, 184.60621643066),
+        RequiredPin = 9423,
+        ID = 7,
+        distance = 2,
+}
+
+    Config.Stash[#Config.Stash + 1] = {
+        StashEntry = vector3(-1289.9039306641, -820.24542236328, 14.571745872498),
+        RequiredPin = 8349,
+        ID = 8,
+        distance = 2,
+}
+
+    Config.Stash[#Config.Stash + 1] = {
+        StashEntry = vector3(-1393.6773681641, -317.74969482422, 56.547580718994),
+        RequiredPin = 6981,
+        ID = 9,
+        distance = 2,
+}
+
+    Config.Stash[#Config.Stash + 1] = {
+        StashEntry = vector3(-1292.4998779297, -602.34533691406, 42.074844360352),
+        RequiredPin = 1020,
+        ID = 10,
+        distance = 2,
+}
+
+    Config.Stash[#Config.Stash + 1] = {
+        StashEntry = vector3(-583.02087402344, 228.12707519531, 79.423408508301),
+        RequiredPin = 0491,
+        ID = 11,
+        distance = 2,
+}
+
+    Config.Stash[#Config.Stash + 1] = {
+        StashEntry = vector3(236.61772155762, -1500.2281494141, 29.291656494141),
+        RequiredPin = 6929,
+        ID = 12,
+        distance = 2,
+}
+
+    Config.Stash[#Config.Stash + 1] = {
+        StashEntry = vector3(-1501.2553710938, -294.87576293945, 48.756126403809),
+        RequiredPin = 4289,
+        ID = 13,
+        distance = 2,
+}
+
+    Config.Stash[#Config.Stash + 1] = {
+        StashEntry = vector3(-296.27230834961, -1295.7854003906, 31.260372161865),
+        RequiredPin = 3560,
+        ID = 14,
+        distance = 2,
+}
+
+    Config.Stash[#Config.Stash + 1] = {
+        StashEntry = vector3(-930.40539550781, 19.431835174561, 48.532962799072),
+        RequiredPin = 9264,
+        ID = 15,
+        distance = 2,
+}
+
+    Config.Stash[#Config.Stash + 1] = {
+        StashEntry = vector3(-1370.3436279297, -503.00988769531, 33.157901763916),
+        RequiredPin = 0512,
+        ID = 16,
+        distance = 2,
+}
+
+    Config.Stash[#Config.Stash + 1] = {
+        StashEntry = vector3(1510.4016113281, 6326.0004882813, 24.606925964355),
+        RequiredPin = 8467,
+        ID = 17,
+        distance = 2,
+}
+
+    Config.Stash[#Config.Stash + 1] = {
+        StashEntry = vector3(-1305.3828125, -803.05615234375, 17.576389312744),
+        RequiredPin = 4893,
+        ID = 18,
+        distance = 2,
+}
+
+    Config.Stash[#Config.Stash + 1] = {
+        StashEntry = vector3(-1098.2059326172, -1678.6110839844, 4.3537006378174),
+        RequiredPin = 0747,
+        ID = 19,
+        distance = 2,
+}
+
+    Config.Stash[#Config.Stash + 1] = {
+        StashEntry = vector3(-39.353130340576, -614.30969238281, 35.269248962402),
+        RequiredPin = 6983,
+        ID = 20,
+        distance = 2,
+}
+
+    Config.Stash[#Config.Stash + 1] = {
+        StashEntry = vector3(-675.88836669922, -884.90466308594, 24.44864654541),
+        RequiredPin = 4862,
+        ID = 21,
+        distance = 2,
+}
+
+    Config.Stash[#Config.Stash + 1] = {
+        StashEntry = vector3(-1541.9200439453, 409.64477539063, 109.87671661377),
+        RequiredPin = 3940,
+        ID = 22,
+        distance = 2,
+}
+
+    Config.Stash[#Config.Stash + 1] = {
+        StashEntry = vector3(283.78604125977, 47.488121032715, 92.660446166992),
+        RequiredPin = 0225,
+        ID = 23,
+        distance = 2,
+}
+
+    Config.Stash[#Config.Stash + 1] = {
+        StashEntry = vector3(-1068.4591064453, -1648.1573486328, 4.856086730957),
+        RequiredPin = 5288,
+        ID = 24,
+        distance = 2,
+}
+
+    Config.Stash[#Config.Stash + 1] = {
+        StashEntry = vector3(-536.77325439453, 477.27633666992, 103.19362640381),
+        RequiredPin = 2820,
+        ID = 25,
+        distance = 2,
+}
+
+    Config.Stash[#Config.Stash + 1] = {
+        StashEntry = vector3(1134.6833496094, -789.12744140625, 57.601161956787),
+        RequiredPin = 7456,
+        ID = 26,
+        distance = 2,
+}
+
+    Config.Stash[#Config.Stash + 1] = {
+        StashEntry = vector3(212.84243774414, 2.4401836395264, 79.307914733887),
+        RequiredPin = 2801,
+        ID = 27,
+        distance = 2,
+}
+
+    Config.Stash[#Config.Stash + 1] = {
+        StashEntry = vector3(-43.892288208008, -1241.9962158203, 29.335014343262),
+        RequiredPin = 2552,
+        ID = 28,
+        distance = 2,
+}
+
+    Config.Stash[#Config.Stash + 1] = {
+        StashEntry = vector3(-930.25305175781, -1101.3370361328, 2.1718454360962),
+        RequiredPin = 6218,
+        ID = 29,
+        distance = 2,
+}
+
+    Config.Stash[#Config.Stash + 1] = {
+        StashEntry = vector3(-715.11541748047, -713.41522216797, 29.130529403687),
+        RequiredPin = 4371,
+        ID = 30,
+        distance = 2,
+}
+
+    Config.Stash[#Config.Stash + 1] = {
+        StashEntry = vector3(-1108.4691162109, -1643.5771484375, 4.6405334472656),
+        RequiredPin = 1830,
+        ID = 31,
+        distance = 2,
+}
+
+    Config.Stash[#Config.Stash + 1] = {
+        StashEntry = vector3(-1328.4887695313, -767.07153320313, 19.424480438232),
+        RequiredPin = 1428,
+        ID = 32,
+        distance = 2,
+}
+
+    Config.Stash[#Config.Stash + 1] = {
+        StashEntry = vector3(-831.45959472656, -864.38116455078, 20.708019256592),
+        RequiredPin = 0397,
+        ID = 33,
+        distance = 2,
+}
+
+    Config.Stash[#Config.Stash + 1] = {
+        StashEntry = vector3(-1602.5443115234, 206.5980682373, 59.274566650391),
+        RequiredPin = 2885,
+        ID = 34,
+        distance = 2,
+}
+
+    Config.Stash[#Config.Stash + 1] = {
+        StashEntry = vector3(943.28173828125, -1697.5383300781, 30.256008148193),
+        RequiredPin = 0313,
+        ID = 35,
+        distance = 2,
+}
